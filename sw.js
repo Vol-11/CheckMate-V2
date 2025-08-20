21f99fd90c9b328e58f6a5a2c240fbb06b5869df
harumaki4649 <support@disnana.com>
2025-08-20 21:21:49 +0900

    no message

--- sw.js
+++ sw.js
@@ -4,12 +4,12 @@ const CACHE_NAME = 'wasuremono-pro-v2.2';
 /* オフラインでも必要なファイルを列挙 */
 const urlsToCache = [
   '/',                       // index.html
-  '/CheckMate-V2',             // GitHub Pages のルート（上と同じ）
-  '/CheckMate-V2index.html',
-  '/CheckMate-V2style.css',
-  '/CheckMate-V2main.js',
-  // '/CheckMate-V2icons/icon-192.png',
-  '/CheckMate-V2icons/icon-512.png',
+  '/CheckMate-V2/',             // GitHub Pages のルート（上と同じ）
+  '/CheckMate-V2/index.html',
+  '/CheckMate-V2/style.css',
+  '/CheckMate-V2/main.js',
+  // '/CheckMate-V2/icons/icon-192.png',
+  '/CheckMate-V2/icons/icon-512.png',
   'https://unpkg.com/quagga@0.12.1/dist/quagga.min.js'
 ];
 
