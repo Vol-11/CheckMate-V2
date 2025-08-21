c2d4a5c11054233a8883f2520237e8ea619ab3be
harumaki4649 <support@disnana.com>
2025-08-21 16:35:05 +0900

    global変数用.jsを作成

---
+++ js/global.js
@@ -0,0 +1,10 @@
+// グローバル変数
+let items = [];
+let currentDay = null;
+let currentCategory = 'all';
+let sortBy = 'name';
+let isScanning = false;
+let isCheckScanning = false;
+let editingItem = null;
+let checkMode = 'manual';
+let scanResults = new Map();
