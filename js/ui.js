0d0dc4d1b91f50acf77101746f6d5b5aa3dd5538
harumaki4649 <support@disnana.com>
2025-08-21 16:30:34 +0900

    ui用jsを作成

---
+++ js/ui.js
@@ -0,0 +1,4 @@
+const tabs = document.querySelectorAll('.tab-btn');
+const tabContents = document.querySelectorAll('.tab-content');
+const status = document.getElementById('status');
+const checkStatus = document.getElementById('check-status');
